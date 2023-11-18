2eb90635cb945d96053231f83b47fcf887c44bb2
CHANDINI200 <94101202+CHANDINI200@users.noreply.github.com>
2023-11-18 11:27:26 +0530

    Update Palindrome.java

--- Palindrome.java
+++ Palindrome.java
@@ -1,23 +1,35 @@
-public class Palindrome {
- 
+import java.util.Scanner;
+
+public class PalindromeChecker {
     public static void main(String[] args) {
- 
-        int rem, rev= 0, temp;
-        int n=121; 
- 
-        temp = n;
- 
-        while( n != 0 )
-        {
-            rem= n % 10;
-            rev= rev * 10 + rem;
-            n=n/10;
+        Scanner scanner = new Scanner(System.in);
+
+        // Input: Get the number from the user
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+
+        // Check if the number is a palindrome
+        if (isPalindrome(number)) {
+            System.out.println(number + " is a palindrome.");
+        } else {
+            System.out.println(number + " is not a palindrome.");
         }
- 
-        
-        if (temp == rev)
-            System.out.println(temp + " is a palindrome.");
-        else
-            System.out.println(temp + " is not a palindrome.");
+
+        scanner.close();
+    }
+
+    // Function to check if a number is a palindrome
+    private static boolean isPalindrome(int num) {
+        int originalNum = num;
+        int reversedNum = 0;
+
+        while (num > 0) {
+            int digit = num % 10;
+            reversedNum = reversedNum * 10 + digit;
+            num /= 10;
+        }
+
+        // Check if the reversed number is equal to the original number
+        return originalNum == reversedNum;
     }
 }
